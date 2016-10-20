995aedfd7915d4a364b891bf9f305a0187a72027
pumasuede <yuxiao1985@hotmail.com>
2016-10-19 20:42:51 -0700

    Auto detect the path of protoc

--- build.sh
+++ build.sh
@@ -1,11 +1,20 @@
-#!/bin/bash
+#!/bin/bash --login
 
 cd `dirname $0`
-type protoc &> /dev/null  || { echo >&2 "protoc does not exist, please install it. Aborting."; exit 1; }
+shopt -s  expand_aliases
+shopt expand_aliases
+gpb_info=`type protoc`
+
+[ $? -eq 1 ] && { echo >&2 "protoc does not exist, please install it. Aborting."; exit 1; }
 protoc --proto_path=./src/proto --cpp_out ./src/pbout ./src/proto/*.proto
 
-if [ ! -d build ]; then mkdir -p build/{src,test} ; fi
+gpb_dir=`echo $gpb_info|sed 's/.*[ \`]\//\//g'|sed s/\'//g`
+for i in [1 2]; do
+    gpb_dir=`dirname $gpb_dir`
+done
+export GPB_DIR=$gpb_dir
 
+if [ ! -d build ]; then mkdir -p build/{src,test} ; fi
 cd build
 
 cd src
